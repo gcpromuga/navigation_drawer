b404fe58899f8f8cddfe56734a793bed6948aef0
Gene Romuga <gcpromuga@letran-calamba.edu.ph>
2022-03-23 15:40:47 +0800

    Update fragments

--- app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.navigationdrawer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     MaterialToolbar toolbar;
     DrawerLayout drawerLayout;
@@ -24,15 +26,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
         frameLayout = findViewById(R.id.main_framelayout);
         drawerLayout = findViewById(R.id.drawerLayout);
         navigationView = findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
         drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();;
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(
+                    R.id.main_framelayout,
+                    new HomeFragment()
+            ).commit();
+            navigationView.setCheckedItem(R.id.home);
+        }
+
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+       switch(item.getItemId()){
+           case R.id.home:
+               getSupportFragmentManager().beginTransaction().replace(
+                       R.id.main_framelayout,
+                       new HomeFragment()
+               ).commit();
+               break;
+       }
+       return true;
+    }
+
 }
